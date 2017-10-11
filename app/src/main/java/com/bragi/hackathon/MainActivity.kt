4a465eadc4d8277aee9c6712a2cc22b2eef92611
micotom <funglejunk.development@googlemail.com>
2017-10-11 20:40:19 +0200

    extracted observable callbacks

--- app/src/main/java/com/bragi/hackathon/MainActivity.kt
+++ app/src/main/java/com/bragi/hackathon/MainActivity.kt
@@ -15,32 +15,39 @@ class MainActivity : AppCompatActivity() {
         DashConnector.lateInit(this)
 
         connect_button.isEnabled = false
-        DashChannel.sdkStateSubjectObservable.observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    if (it == DashChannel.SdkState.INITIALIZED) {
-                        connect_button.isEnabled = true
-                        connect_button.setOnClickListener {
-                            DashConnector.connect(this)
-                        }
-                    }
-                    else {
-                        connect_button.isEnabled = false
-                    }
-                }
+        DashChannel.sdkStateSubjectObservable.observeOn(AndroidSchedulers.mainThread()).subscribe {
+            onNewSdkState(it)
+        }
 
         DashChannel.connectionStateObservable.observeOn(AndroidSchedulers.mainThread()).subscribe {
-            when (it) {
-                DashChannel.ConnectionState.CONNECTED -> {
-                    dash_connection_status_text.text = "Connected"
-                    DashConnector.readAcc()
-                    DashConnector.readHr()
-                }
-                else -> {} // ignore
-            }
+            onNewConnectionState(it)
         }
 
         DataChannel.incomingMessageObservable.subscribe {
             data_text.text = it
+        }
+    }
+
+    private fun onNewConnectionState(it: DashChannel.ConnectionState?) {
+        when (it) {
+            DashChannel.ConnectionState.CONNECTED -> {
+                dash_connection_status_text.text = "Connected"
+                DashConnector.readAcc()
+                DashConnector.readHr()
+            }
+            else -> {
+            } // ignore
+        }
+    }
+
+    private fun onNewSdkState(it: DashChannel.SdkState?) {
+        if (it == DashChannel.SdkState.INITIALIZED) {
+            connect_button.isEnabled = true
+            connect_button.setOnClickListener {
+                DashConnector.connect(this)
+            }
+        } else {
+            connect_button.isEnabled = false
         }
     }
 
